6046ec6b812ce23843551c7be9429eaa2f9d4485
Agata Lewandowska <lewandowskaagata15@gmail.com>
2020-11-21 11:01:06 +0400

    8.7 done / numbers not changing

--- src/js/script.js
+++ src/js/script.js
@@ -63,7 +63,9 @@
       thisProduct.getElements();
       thisProduct.initAccordion();
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
+      
       
 
       console.log('new Product:', thisProduct);
@@ -94,8 +96,9 @@
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
     }
-
+   
     initAccordion() {
       const thisProduct = this;
 
@@ -147,7 +150,14 @@
         thisProduct.processOrder();
       });
     }
+    initAmountWidget(){
+      const thisProduct = this;
 
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+      thisProduct.amountWidgetElem.addEventListener('updated', function() {
+        thisProduct.processOrder();
+      });
+    }
     processOrder(){
       const thisProduct = this;
 
@@ -181,29 +191,90 @@
           /* END ELSE IF: if option is not selected and option is default */
           }
           /* all images found in thisProduct.imageWrapper for selector: dot, paramId, dash, optionId */
-        const allImages = thisProduct.imageWrapper.querySelectorAll('.' + paramId + '-' + optionId);
+          const allImages = thisProduct.imageWrapper.querySelectorAll('.' + paramId + '-' + optionId);
         
-        /* START LOOP: display or remove ingredient on salad or pizza if option was selected or removed*/ 
-        for(let image of allImages){
-        if(optionSelected) {
-          image.classList.add(classNames.menuProduct.imageVisible); 
-       } else {
-           image.classList.remove(classNames.menuProduct.imageVisible);
-        }
+          /* START LOOP: display or remove ingredient on salad or pizza if option was selected or removed*/ 
+          for(let image of allImages){
+            if(optionSelected) {
+              image.classList.add(classNames.menuProduct.imageVisible); 
+            } else {
+              image.classList.remove(classNames.menuProduct.imageVisible);
+            }
           /*END LOOP: if option was selected */
-        }
+          }
         /* END LOOP: for each optionId in param.options */
         }
       /* END LOOP: for each paramId in thisProduct.data.params */
       }
+      /* multiply price by amount */
+      price *= thisProduct.amountWidget.value;
+
       /* set the contents of thisProduct.priceElem to be the value of variable price */
       thisProduct.priceElem.innerHTML = price;
+    }
+  }
+
+  class AmountWidget{
+    constructor(element){
+      const thisWidget = this;
+
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.value = settings.amountWidget.defaultValue;
+
+      console.log('AmountWidget:', thisWidget);
+      console.log('constructor arguments:', element);
+    }
+    getElements(element){
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+    
+    setValue(value){
+      const thisWidget = this;
+
+      const newValue = parseInt(value);
+
+      /* TO DO: Add validation */ 
+      
+      if(newValue != thisWidget.value && newValue >= settings.amountWidget.defaultMin <= settings.amountWidget.defaultMax) {
+        thisWidget.value = newValue; 
+        thisWidget.announce();
+      }
+      thisWidget.input.value = thisWidget.value;
+    }
+
+    initActions(){
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function() {
+        thisWidget.setValue(thisWidget.input.value);
+      });
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+      thisWidget.linkIncrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+    }
+
+    announce(){
+      const thisWidget = this;
+
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
     }
   }
   const app = {
     initMenu: function () {
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
+      //  console.log('thisApp.data:', thisApp.data);
 
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
@@ -223,7 +294,7 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
-
+    
       thisApp.initData();
       thisApp.initMenu();
     },

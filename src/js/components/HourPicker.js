11253fdc52235b4736bc73ba5ffde385b4e3b0f0
Agata Lewandowska <lewandowskaagata15@gmail.com>
2020-12-16 21:39:39 +0400

    10.3 DatePicker, HourPicker not working

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -2,17 +2,38 @@ import {select, settings} from '../settings.js';
 import utils from '../utils.js';
 import BaseWidget from './BaseWidget.js';
 
-class HourPicker {
+class HourPicker extends BaseWidget {
   constructor (wrapper) {
-   super(wrapper, settings.hour.open);
+    super(wrapper, settings.hour.open);
 
-   thisWidget.dom.input = 
-   thisWidget.dom.output = 
+    const thisWidget = this;
+
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.input);
+    thisWidget.dom.output = thisWidget.dom.wrapper.querySelector(select.widgets.hourPicker.output);
+
+    thisWidget.initPlugin();
 
   }
-}
-  initPlugin (){
+  initPlugin () {
+    const thisWidget = this; 
 
+    rangeSlider.create(thisWidget.dom.input);
+    
+    thisWidget.dom.input.addEventListener('input', function(){
+      thisWidget.value = thisWidget.dom.input.value;
+    });
+  }
+  parseValue(value) { 
+    return utils.numberToHour(value);
+  }
+
+  isValid() {
+    return true;
+  }
+  renderValue() {
+    const thisWidget = this; 
+
+    thisWidget.dom.output.innerHTML = thisWidget.value;
   }
 }
 
